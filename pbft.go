053e5ee76c84c610721d760218d991b0f9c34f0c
dongjize <justecommeca@163.com>
2018-10-01 03:02:59 +1000

    pbft

--- pbft.go
+++ pbft.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"crypto"
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -16,21 +23,27 @@ type nodeInfo struct {
 
 // The address map of the 4 countries
 var nodeTable = make(map[string]string)
+var privK *rsa.PrivateKey
+var pubK *rsa.PublicKey
+var sessionK = "e3988cce1bdcd1db1b0a1313e598b12040d4e16f"
 
 func main() {
 
 	userId := os.Args[1]
-	fmt.Println(userId)
+	fmt.Println("node " + userId)
 
 	//./main Arsenal
 
 	// initiate the addresses of the four countries
 	nodeTable = map[string]string{
-		"Arsenal":   "localhost:1111",
-		"Chelsea":   "localhost:1112",
-		"Liverpool": "localhost:1113",
-		"ManUtd":    "localhost:1114",
+		"0": "localhost:1110",
+		"1": "localhost:1111",
+		"2": "localhost:1112",
+		"3": "localhost:1113",
 	}
+
+	privK, _ = rsa.GenerateKey(rand.Reader, 1024)
+	pubK = &privK.PublicKey
 
 	node := nodeInfo{userId, nodeTable[userId], nil}
 	fmt.Println(node)
@@ -50,33 +63,80 @@ func main() {
 
 }
 
+func generateRSASignature(msg string) ([]byte, []byte, *rsa.PSSOptions) {
+	plaintext := []byte(msg)
+	h := md5.New()
+	h.Write(plaintext)
+
+	hashed := h.Sum(nil)
+	opts := &rsa.PSSOptions{SaltLength: rsa.PSSSaltLengthAuto, Hash: crypto.MD5}
+	sign, _ := rsa.SignPSS(rand.Reader, privK, crypto.MD5, hashed, opts)
+	return hashed, sign, opts
+}
+
+func generateMAC(data, key []byte) []byte {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(data)
+	return mac.Sum(nil)
+}
+
+func checkMAC(message, messageMAC, key []byte) bool {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(message)
+	expectedMAC := mac.Sum(nil)
+	return hmac.Equal(messageMAC, expectedMAC)
+}
+
 func (node *nodeInfo) request(writer http.ResponseWriter, request *http.Request) {
 	// receive and parse the params
 	request.ParseForm()
-	//如果有参数值，则继续处理
-	if len(request.Form["warTime"]) > 0 {
+	if len(request.Form["message"]) > 0 {
 		node.writer = writer
-		//激活主节点后，广播给其他节点,通过Arsenal向其他节点做广播
-		node.broadcast(request.Form["warTime"][0], "/prePrepare")
+		hashed, sign, opts := generateRSASignature(request.Form["message"][0])
+		node.broadcast0(sign, "/prePrepare", hashed, opts)
 	}
 
 }
 
 //由主节点向其他节点做广播
-func (node *nodeInfo) broadcast(msg string, path string) {
-	//遍历所有的国家
-	for nodeId, url := range nodeTable {
+func (node *nodeInfo) broadcast0(sign []byte, path string, hashed []byte, opts *rsa.PSSOptions) {
+	e := rsa.VerifyPSS(pubK, crypto.MD5, hashed, []byte(sign), opts)
+
+	if e != nil {
+		fmt.Println("authentication failed")
+		return
+	}
+
+	// loop through all replicas
+	for nodeId, _url := range nodeTable {
 
 		if nodeId == node.id {
 			continue
 		}
-		//调用Get请求
-		//http.Get("http://localhost:1112/prePrepare?warTime=8888&nodeId=Arsenal")
-		http.Get("http://" + url + path + "?warTime=" + msg + "&nodeId=" + node.id)
+		message := "authenticating message from " + node.id + " to " + nodeId
+		mac := generateMAC([]byte(message), []byte(sessionK))
+		data := url.Values{"message": {message}, "mac": {string(mac)}, "nodeId": {node.id}}
+		http.PostForm("http://"+_url+path, data)
 	}
 
 }
 
+func (node *nodeInfo) broadcast(path string, message []byte, mac []byte) {
+	for nodeId, _url := range nodeTable {
+
+		if nodeId == node.id {
+			continue
+		}
+		isMACEqual := checkMAC(message, []byte(mac), []byte(sessionK))
+		if isMACEqual {
+			haha := "http://" + _url + path
+			data := url.Values{"message": {string(message)}, "mac": {string(mac)}, "nodeId": {node.id}}
+			http.PostForm(haha, data)
+		}
+	}
+}
+
+// Primary 0 receives the request from C and multicasts it to backups.
 func (node *nodeInfo) prePrepare(writer http.ResponseWriter, request *http.Request) {
 	if node.writer == nil {
 		node.writer = writer
@@ -84,30 +144,43 @@ func (node *nodeInfo) prePrepare(writer http.ResponseWriter, request *http.Reque
 
 	// receive and parse the params
 	request.ParseForm()
-	//fmt.Println("hello world")
 	// distribute again
-	if len(request.Form["warTime"]) > 0 {
+	if len(request.PostFormValue("message")) > 0 && len(request.PostFormValue("mac")) > 0 {
 		// distribute to the other 3 nodes
-		node.broadcast(request.Form["warTime"][0], "/prepare")
+		message := request.PostFormValue("message")
+		mac := request.PostFormValue("mac")
+		//nodeId := request.Form["nodeId"][0]
+		isMACEqual := checkMAC([]byte(message), []byte(mac), []byte(sessionK))
+		if isMACEqual {
+			newMsg := "message for prepare"
+			newMAC := generateMAC([]byte(newMsg), []byte(sessionK))
+			node.broadcast("/prepare", []byte(newMsg), newMAC)
+		} else {
+			fmt.Println("prePrepare: authentication failed")
+		}
 	}
 
 }
 
+// Replicas execute the request and then re-broadcast the result to each other
 func (node *nodeInfo) prepare(writer http.ResponseWriter, request *http.Request) {
-
-	request.ParseForm()
-	//调用验证
-	if len(request.Form["warTime"]) > 0 {
-		fmt.Println(request.Form["warTime"][0])
+	if node.writer == nil {
+		node.writer = writer
 	}
-	if len(request.Form["nodeId"]) > 0 {
-		fmt.Println(request.Form["nodeId"][0])
+	request.ParseForm()
+	if len(request.PostFormValue("message")) > 0 {
+		fmt.Println(request.PostFormValue("message"))
+	}
+	if len(request.PostFormValue("mac")) > 0 {
+		fmt.Println(request.PostFormValue("mac"))
+	}
+	if len(request.PostFormValue("nodeId")) > 0 {
+		fmt.Println(request.PostFormValue("nodeId"))
 	}
 
 	node.authentication(request)
 }
 
-var authenticationSuccess = true
 var authenticationMap = make(map[string]string)
 
 // receive other nodes' info except that of self
@@ -116,23 +189,44 @@ func (node *nodeInfo) authentication(request *http.Request) {
 	//接收参数
 	request.ParseForm()
 
-	if authenticationSuccess != false {
-		if len(request.Form["nodeId"]) > 0 {
-			authenticationMap[request.Form["nodeId"][0]] = "ok"
-		}
+	if len(request.Form["nodeId"]) > 0 {
+		authenticationMap[request.PostFormValue("nodeId")] = "ok"
 	}
 
 	// if the received msg count > node table count * 1/3
 	if len(authenticationMap) > len(nodeTable)/3 {
-		// then PBFT consensus is achieved; commit the feedback to browser
-		node.broadcast(request.Form["warTime"][0], "/commit")
-
+		if len(request.PostFormValue("message")) > 0 && len(request.PostFormValue("mac")) > 0 {
+			message := request.PostFormValue("message")
+			mac := request.PostFormValue("mac")
+			isMACEqual := checkMAC([]byte(message), []byte(mac), []byte(sessionK))
+			if isMACEqual {
+				// then PBFT consensus is achieved; commit the feedback to browser
+				newMsg := "message for commit"
+				newMAC := generateMAC([]byte(newMsg), []byte(sessionK))
+				node.broadcast("/commit", []byte(newMsg), []byte(newMAC))
+			} else {
+				fmt.Println("prePrepare: authentication failed")
+			}
+		}
 	}
+
 }
 
+// commit the feedback to the browser
 func (node *nodeInfo) commit(writer http.ResponseWriter, request *http.Request) {
-
-	// commit the feedback to the browser
-	io.WriteString(node.writer, "ok")
+	if node.writer == nil {
+		node.writer = writer
+	}
+	if len(request.PostFormValue("message")) > 0 && len(request.PostFormValue("mac")) > 0 {
+		message := request.PostFormValue("message")
+		mac := request.PostFormValue("mac")
+		isMACEqual := checkMAC([]byte(message), []byte(mac), []byte(sessionK))
+		if isMACEqual {
+			// then PBFT consensus is achieved; commit the feedback to browser
+			io.WriteString(node.writer, "authentication successful from node "+request.PostFormValue("nodeId")+"\n")
+		} else {
+			fmt.Println("commit: authentication failed")
+		}
+	}
 
 }
